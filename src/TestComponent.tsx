b86b8f7193950e94440de2973d91fee2618e4389
Jakub Pacewicz <pecet3107@gmail.coim>
2023-03-13 22:28:34 +0100

    edited interface and content

--- src/TestComponent.tsx
+++ src/TestComponent.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
+
 export interface TextInputProps {
     text: string;
+    sayHello: boolean;
+    // Zobacz, w app, jeżeli usuniesz text lub sayHello z propsów, wtedy jest błąd. 
+    // Aby jakaś zmienna była opcjonalna to musisz dać znak zapytania, jak poniżej.
+    optionalString?: string;
+    // tak funkcje sie deklaruje
+    anyFunction?: () => void;
 }
 
-const TestComponent: React.FC<TextInputProps> = ({text}) =>{
+const TestComponent: React.FC<TextInputProps> = ({text, sayHello}) =>{
   return (
+    <>
     <p>
         {text}
     </p>
+    {sayHello && <h1>Hello</h1>}
+    </>
   );
 }
 
