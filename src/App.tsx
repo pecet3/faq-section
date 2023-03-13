a145eb421a17d5e8fb900420b0bbf5ffafca05fa
Jakub Pacewicz <pecet3107@gmail.coim>
2023-03-13 22:28:22 +0100

    implemented new component, removed unneccesary content

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import TestComponent from "./TestComponent";
+import {TestComponent2} from "./TestComponent2";
 
 function App() {
   return (
     <>
-    <TestComponent text="witaj ojcze, to jest testowy komponent, zobacz go jak dokładnie wygląda, posiada interface, gdzie określasz w nim typy zmiennych"/>
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-    </>
+    <TestComponent 
+    text="witaj ojcze, to jest testowy komponent, zobacz go jak dokładnie wygląda, posiada interface, gdzie określasz w nim typy zmiennych"
+    sayHello={true}
+    />
+    <TestComponent2
+    title="Hello, this is TestComponent2"
+    luckyNumber={888}
+    />
+  </>
   );
 }
 
