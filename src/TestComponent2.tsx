c7c9f3b39531245da90ea3e02c692705f132f452
Jakub Pacewicz <pecet3107@gmail.coim>
2023-03-14 23:35:32 +0100

    created function renderList, and implemented it into JSX

--- src/TestComponent2.tsx
+++ src/TestComponent2.tsx
@@ -25,12 +25,24 @@ export const TestComponent2 = ({ title, luckyNumber }: PropsInterface) => {
     counter: 0,
   });
 
+  const renderList = () => {
+    return data.map((person) => {
+      return (
+        <li key={person.name}>
+          <h2>{person.name}</h2>
+          <h3>{person.age} years old</h3>
+          <h4>{person.note}</h4>
+        </li>
+      );
+    });
+  };
+
   return (
     <>
       <div>
         <h2>{title}</h2>
         <p>Your lucky number is: {luckyNumber}</p>
-        <h1>{}</h1>
+        <ul>{renderList()}</ul>
       </div>
     </>
   );
