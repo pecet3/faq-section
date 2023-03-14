b441aa344bc6a9a3104abbd460b372b999bbcf2a
Jakub Pacewicz <pecet3107@gmail.coim>
2023-03-14 23:14:06 +0100

    formatted code

--- src/TestComponent.tsx
+++ src/TestComponent.tsx
@@ -1,38 +1,39 @@
-import React from 'react';
+import React from "react";
 
-export  interface PersonalData {
-    name: string;
-    age?: number;
+export interface PersonalData {
+  name: string;
+  age?: number;
 }
 
-
 export interface TextInputProps {
-    text: string;
-    sayHello: boolean;
-    // Zobacz, w app, jeżeli usuniesz text lub sayHello z propsów, wtedy jest błąd. 
-    // Aby jakaś zmienna była opcjonalna to musisz dać znak zapytania, jak poniżej.
-    optionalString?: string;
-    // tak funkcje sie deklaruje
-    anyFunction?: () => void;
-    // w tym interface dodałem interface PersonalData
-    personalData?: PersonalData;
+  text: string;
+  sayHello: boolean;
+  // Zobacz, w app, jeżeli usuniesz text lub sayHello z propsów, wtedy jest błąd.
+  // Aby jakaś zmienna była opcjonalna to musisz dać znak zapytania, jak poniżej.
+  optionalString?: string;
+  // tak funkcje sie deklaruje
+  anyFunction?: () => void;
+  // w tym interface dodałem interface PersonalData
+  personalData?: PersonalData;
 }
 
 //                                                                    \/ musze podac nazwe w propsach tego interface personalData
 
-const TestComponent: React.FC<TextInputProps> = ({text, sayHello, personalData}) =>{
+const TestComponent: React.FC<TextInputProps> = ({
+  text,
+  sayHello,
+  personalData,
+}) => {
   return (
     <>
-    <p>
-        {text}
-    </p>
-    {sayHello && <h1>Hello</h1>}
-    <div>
-        imię: <b>{personalData.name}</b> <br/>
+      <p>{text}</p>
+      {sayHello && <h1>Hello</h1>}
+      <div>
+        imię: <b>{personalData.name}</b> <br />
         wiek: <i>{personalData.age}</i>
-    </div>
+      </div>
     </>
   );
-}
+};
 
 export default TestComponent;
