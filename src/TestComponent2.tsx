b38a701e0ac0ba195fb1afb3fc130e24d4dcf1be
Jakub Pacewicz <pecet3107@gmail.coim>
2023-03-14 23:14:20 +0100

    formatted code, added useState

--- src/TestComponent2.tsx
+++ src/TestComponent2.tsx
@@ -1,15 +1,26 @@
+import { useState, useEffect } from "react";
+
 interface PropsInterface {
-    title: string;
-    luckyNumber: number;
+  title: string;
+  luckyNumber: number;
 }
 
+interface Button {
+  counter: number;
+}
 
-export const TestComponent2 = ({title, luckyNumber}: PropsInterface) => {
+export const TestComponent2 = ({ title, luckyNumber }: PropsInterface) => {
+  const [counter, setCounter] = useState<Button>({
+    counter: 0,
+  });
 
-    return(
-        <div>
+  return (
+    <>
+      <div>
         <h2>{title}</h2>
         <p>Your lucky number is: {luckyNumber}</p>
-        </div>
-    )
-}
+        <h1>{}</h1>
+      </div>
+    </>
+  );
+};
