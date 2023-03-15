01eb338752152462a6cad91441a9c43c0a8a3c5b
Jakub Pacewicz <pecet3107@gmail.coim>
2023-03-15 14:46:07 +0100

    created useState, inputs and handle function

--- src/AddToList.tsx
+++ src/AddToList.tsx
@@ -1,10 +1,45 @@
 import React, { useState } from "react";
 
 const AddToList: React.FC = () => {
+  const [input, setInput] = useState({
+    name: "",
+    age: "",
+    note: "",
+  });
+
+  const onChangeInput = (
+    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    setInput({
+      ...input,
+      [event.target.name]: event.target.value,
+    });
+    console.log(input);
+  };
+
   return (
     <>
       <p>AddToList</p>
-      <input type="text" />
+      <input
+        type="text"
+        name="name"
+        onChange={onChangeInput}
+        value={input.name}
+        placeholder="podaj imie"
+      />
+      <input
+        type="text"
+        name="age"
+        onChange={onChangeInput}
+        value={input.age}
+        placeholder="podaj wiek"
+      />
+      <textarea
+        name="note"
+        onChange={onChangeInput}
+        value={input.note}
+        placeholder="notatka"
+      />
     </>
   );
 };
