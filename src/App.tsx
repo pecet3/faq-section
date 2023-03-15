8e20446230680bedaf5707c4ab6ff06c9758b85d
Jakub Pacewicz <pecet3107@gmail.coim>
2023-03-15 14:20:35 +0100

    added AddToList

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,20 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 import TestComponent from "./TestComponent";
-import {TestComponent2} from "./TestComponent2";
+import { TestComponent2 } from "./TestComponent2";
+import AddToList from "./AddToList";
 
 function App() {
   return (
     <>
-    <TestComponent 
-    text="witaj ojcze, to jest testowy komponent, zobacz go jak dokładnie wygląda, posiada interface, gdzie określasz w nim typy zmiennych"
-    sayHello={true}
-    personalData={{name: "Jakub", age: 23}}
-    />
-    <TestComponent2
-    title="Hello, this is TestComponent2"
-    luckyNumber={888}
-    />
-  </>
+      <TestComponent
+        text="witaj ojcze, to jest testowy komponent, zobacz go jak dokładnie wygląda, posiada interface, gdzie określasz w nim typy zmiennych"
+        sayHello={true}
+        personalData={{ name: "Jakub", age: 23 }}
+      />
+      <TestComponent2 title="Hello, this is TestComponent2" luckyNumber={888} />
+      <AddToList />
+    </>
   );
 }
 
