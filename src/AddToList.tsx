577fd629ed792ddf7ff57e0bce1cb3eeb79d673d
Jakub Pacewicz <pecet3107@gmail.coim>
2023-03-15 14:20:24 +0100

    created new component

---
+++ src/AddToList.tsx
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+
+const AddToList: React.FC = () => {
+  return (
+    <>
+      <p>AddToList</p>
+      <input type="text" />
+    </>
+  );
+};
+
+export default AddToList;
